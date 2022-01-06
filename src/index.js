f5e6664ab86995660710ad59ad51468ff86d3f2a
Arash-11 <arashnawy@gmail.com>
2022-01-05 20:28:50 -0400

    fixed some minor bugs and improved code readability

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const outputTextarea = document.querySelector('#output-textarea');
 const toJsonBtn = document.querySelector('[data-convert-to-json]');
 const toIniBtn = document.querySelector('[data-convert-to-ini]');
 
-inputTextarea.value = `; last modified 1 April 2001 by John Doe
+const example = (
+`; last modified 1 April 2001 by John Doe
 [owner]
 name = John Doe
 organization = Acme Widgets Inc.
-file = "payroll.dat"`;
+file = "payroll.dat"`
+);
+
+inputTextarea.value = example;
 
 
 const convertIniToJson = (iniText) => {
@@ -16,13 +20,13 @@ const convertIniToJson = (iniText) => {
 
   // Some OSes seem to use a carriage return character returned by newline so we need to consider \r too.
   iniText.split(/\r?\n/).forEach(line => {
-    let match;
+    let match = null;
     // The use of ^ and $ in the conditionals are to make sure the expression matches the whole line and not just part of it. Leaving these out could result in strange behaviours for some inputs.
     if (match = line.match(/^\[(.*)\]$/)) {
       result[match[1]] = {};
       section = result[match[1]];
-    } else if (match = line.match(/^(\w+)\s?=\s?(.*)$/)) {
-      section[match[1]] = match[2];
+    } else if (match = line.match(/^(\w+)(\s*)?=(\s*)?(.*)$/)) {
+      section[match[1]] = match[4];
     } else if (!(/^\s*([;#].*)?$/).test(line)) {
       // Throw an error if the line is not empty or a comment - i.e. when it's invalid.
       throw new Error(`Line '${line}' is not valid.`);
